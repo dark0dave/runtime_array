1de277521abc7a55c65c3c699e9e8c3e2126fedd
dark0dave <dark0dave@mykolab.com>
2023-10-28 09:51:31 +0100

    feat(references): Instead of pointer for memory safety
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- src/array.rs
+++ src/array.rs
@@ -1,115 +1,71 @@
-use std::{alloc::Layout, fmt::Debug, marker::PhantomData};
+use std::{fmt::Debug, marker::PhantomData, mem::ManuallyDrop, ops::Deref, slice};
 
 use serde::{de::Visitor, Deserialize, Serialize};
 
-use crate::iter::Iter;
-
-#[derive(Copy, Eq, PartialOrd, Ord)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(C, packed)]
-pub struct Array<T: Serialize + for<'a> Deserialize<'a> + Copy> {
-    pub(crate) pointer: *mut T,
+pub struct Array<'a, T: Serialize + Deserialize<'a> + Copy + Deref + PartialOrd> {
+    pub(crate) pointer: &'a [T],
     size: usize,
 }
 
-impl<T: ?Sized + Serialize + for<'a> Deserialize<'a> + Copy> Array<T> {
+impl<'a, T: Serialize + Deserialize<'a> + Copy + Deref + PartialOrd> Array<'a, T> {
     #[inline]
-    pub fn new(size: usize) -> Array<T> {
-        let layout = match Layout::array::<T>(size) {
-            Ok(layout) => layout,
-            Err(_) => panic!("Could not create layout"),
-        };
-        let _ = layout.pad_to_align();
-        unsafe {
-            let ptr = std::alloc::alloc(layout) as *mut T;
-            if ptr.is_null() {
-                panic!("Allocation returned null pointer")
-            } else {
-                Self { pointer: ptr, size }
-            }
+    pub fn new(data: &'a [T]) -> Array<'a, T> {
+        Self {
+            pointer: data,
+            size: data.len(),
         }
-    }
-    #[inline]
-    pub fn iter(&self) -> Iter<T> {
-        Iter::new(self)
     }
     #[inline]
     pub fn size(&self) -> usize {
         self.size
     }
-    /// # Safety
-    ///
-    /// Get pointer to index
-    #[inline]
-    pub unsafe fn get_ptr(&self, index: usize) -> *const T {
-        self.pointer.add(index)
-    }
-    /// # Safety
-    ///
-    /// Get a mutable pointer to index
-    #[inline]
-    pub unsafe fn get_mut_ptr(&self, index: usize) -> *mut T {
-        self.pointer.add(index)
-    }
-    /// # Safety
-    ///
-    /// An unchecked get
-    #[inline]
-    pub unsafe fn get_unchecked(&self, index: usize) -> &T {
-        &(*(self.pointer.add(index)))
-    }
     #[inline]
     pub fn get(&self, index: usize) -> Option<&T> {
-        if self.size <= index {
-            None
-        } else {
-            unsafe { Some(self.get_unchecked(index)) }
-        }
-    }
-    /// # Safety
-    ///
-    /// This method does not check the index bounds,
-    /// so it's more efficient,
-    /// but can produce undefined behaviour
-    #[inline]
-    pub unsafe fn get_mut(&mut self, index: usize) -> &mut T {
-        &mut (*(self.pointer.add(index)))
+        self.pointer.get(index)
     }
 }
 
-impl<'b, T: Serialize + for<'a> Deserialize<'a> + Copy> IntoIterator for &'b Array<T> {
-    type Item = &'b T;
-    type IntoIter = Iter<'b, T>;
-    fn into_iter(self) -> Self::IntoIter {
-        Iter::new(self)
-    }
-}
-
-impl<T: Serialize + for<'a> Deserialize<'a> + Copy> From<Vec<T>> for Array<T> {
+impl<'a, T: Serialize + Deserialize<'a> + Copy + PartialEq + PartialOrd + Deref + PartialOrd>
+    From<Vec<T>> for Array<'a, T>
+{
     fn from(vec: Vec<T>) -> Self {
         let size = vec.len();
-        let mut array = Array::new(size);
-        let mut i = 0_usize;
+        let v: Vec<T> = Vec::with_capacity(size);
+        let mut v = ManuallyDrop::new(v);
+        let ptr: *mut T = v.as_mut_ptr();
+
         unsafe {
-            for item in vec {
-                *array.get_mut(i) = item;
-                i += 1
+            let (_head, aligned, _tail) = vec.align_to::<T>();
+            for (counter, t) in aligned.iter().enumerate() {
+                let tmp = std::ptr::read(t);
+                std::ptr::write(ptr.add(counter), tmp);
             }
+            let data = slice::from_raw_parts(ptr, size);
+            Array::new(data)
         }
-        array
     }
 }
 
-impl<'de, T: Serialize + for<'a> Deserialize<'a> + 'de + Copy> Deserialize<'de> for Array<T> {
+impl<
+        'a,
+        T: Serialize + Deserialize<'a> + 'a + Copy + PartialEq + PartialOrd + Deref + PartialOrd,
+    > Deserialize<'a> for Array<'a, T>
+{
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
-        D: serde::Deserializer<'de>,
+        D: serde::Deserializer<'a>,
     {
-        struct ArrayVisitor<'de, T> {
-            out: PhantomData<&'de T>,
+        struct ArrayVisitor<'a, 'de: 'a, T> {
+            outer: PhantomData<&'de T>,
+            inner: PhantomData<&'a T>,
         }
 
-        impl<'de, T: Serialize + for<'a> Deserialize<'a> + Copy> Visitor<'de> for ArrayVisitor<'de, T> {
-            type Value = Array<T>;
+        impl<'a: 'de, 'de, T: Serialize + Deserialize<'de> + Copy + Deref + PartialOrd> Visitor<'de>
+            for ArrayVisitor<'a, 'de, T>
+        {
+            type Value = Array<'a, T>;
 
             fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
                 write!(formatter, "struct VarriableCharArray")
@@ -120,7 +76,7 @@ impl<'de, T: Serialize + for<'a> Deserialize<'a> + 'de + Copy> Deserialize<'de> 
                 A: serde::de::SeqAccess<'de>,
             {
                 // TODO: Assign in one block
-                let mut destination = Vec::with_capacity(seq.size_hint().unwrap_or(0));
+                let mut destination = vec![];
                 while let Ok(Some(item)) = seq.next_element::<T>() {
                     destination.push(item);
                 }
@@ -128,41 +84,25 @@ impl<'de, T: Serialize + for<'a> Deserialize<'a> + 'de + Copy> Deserialize<'de> 
             }
         }
 
-        let visitor = ArrayVisitor { out: PhantomData };
+        let visitor = ArrayVisitor {
+            outer: PhantomData,
+            inner: PhantomData,
+        };
         deserializer.deserialize_seq(visitor)
     }
 }
 
-impl<T: Serialize + for<'a> Deserialize<'a> + Copy> Serialize for Array<T> {
+impl<
+        'a,
+        'de: 'a,
+        T: Serialize + Deserialize<'a> + Copy + PartialEq + PartialOrd + Deref + PartialOrd,
+    > Serialize for Array<'a, T>
+{
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
-        serializer.collect_seq(self.iter())
-    }
-}
-
-impl<T: Serialize + for<'a> Deserialize<'a> + Copy + PartialEq> PartialEq for Array<T> {
-    fn eq(&self, other: &Self) -> bool {
-        if self.size != other.size {
-            return false;
-        }
-        if !self.into_iter().eq(other.into_iter()) {
-            return false;
-        }
-        true
-    }
-}
-
-impl<T: Serialize + for<'a> Deserialize<'a> + Copy + Debug> Debug for Array<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.into_iter().collect::<Vec<_>>().fmt(f)
-    }
-}
-
-impl<T: Serialize + for<'a> Deserialize<'a> + Copy + Clone> Clone for Array<T> {
-    fn clone(&self) -> Self {
-        *self
+        serializer.collect_seq(self.pointer.iter())
     }
 }
 
@@ -188,13 +128,41 @@ mod tests {
         j: i128,
     }
 
-    #[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq, Eq)]
-    #[repr(C, packed)]
-    struct ArrayExample(Array<Example>);
+    impl Deref for Example {
+        type Target = Example;
 
-    #[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq, Eq)]
+        fn deref(&self) -> &Self::Target {
+            self
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, Serialize, PartialEq)]
     #[repr(C, packed)]
-    struct StackArrayExample(ArrayExample);
+    struct ArrayExample<'a>(Array<'a, Example>);
+
+    impl<'a, 'de: 'a> Deserialize<'de> for ArrayExample<'a> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            Ok(ArrayExample(Array::deserialize::<_>(deserializer).unwrap()))
+        }
+    }
+
+    #[derive(Debug, Clone, Serialize, PartialEq)]
+    #[repr(C, packed)]
+    struct StackArrayExample<'a>(ArrayExample<'a>);
+
+    impl<'a, 'de: 'a> Deserialize<'de> for StackArrayExample<'a> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            Ok(StackArrayExample(
+                ArrayExample::deserialize::<_>(deserializer).unwrap(),
+            ))
+        }
+    }
 
     fn copy_buff_to_struct<T>(buffer: &[u8]) -> T {
         unsafe { std::ptr::read(buffer.as_ptr() as *const _) }
@@ -211,7 +179,7 @@ mod tests {
 
     #[test]
     fn array_size() {
-        assert_eq!(std::mem::size_of::<ArrayExample>(), 16)
+        assert_eq!(std::mem::size_of::<ArrayExample>(), 24)
     }
 
     #[test]
@@ -251,7 +219,7 @@ mod tests {
         let value = serde_json::to_string(&expected).unwrap();
 
         let result: ArrayExample = serde_json::from_slice(value.as_bytes()).unwrap();
-        assert_eq!(expected, result)
+        assert_eq!(expected, result);
     }
 
     #[test]
@@ -274,11 +242,11 @@ mod tests {
         );
         let expected = StackArrayExample(ArrayExample(example));
         let result: StackArrayExample = copy_buff_to_struct(to_u8_slice(&expected));
-        assert_eq!(expected, result)
+        assert_eq!(expected, result);
     }
 
     #[test]
-    fn deserialize_serialize_ten_times() {
+    fn deserialize_serialize_deserialize_serialize() {
         let example: Array<Example> = Array::from(
             (1..10)
                 .map(|i| Example {
@@ -297,11 +265,9 @@ mod tests {
         );
         let expected = StackArrayExample(ArrayExample(example));
         let mut result: StackArrayExample = copy_buff_to_struct(to_u8_slice(&expected));
-        for _i in 0..10 {
-            let value = serde_json::to_string(&expected).unwrap();
-            result = serde_json::from_slice(value.as_bytes()).unwrap();
-            result = copy_buff_to_struct(to_u8_slice(&result));
-        }
+        let value = serde_json::to_string(&result).unwrap();
+        result = serde_json::from_slice(value.as_bytes()).unwrap();
+        result = copy_buff_to_struct(to_u8_slice(&result));
         assert_eq!(
             expected.0 .0.get(0),
             Some(&Example {
@@ -332,6 +298,6 @@ mod tests {
                 j: i128::MAX,
             })
         );
-        assert_eq!(result, expected)
+        assert_eq!(result, expected);
     }
 }
