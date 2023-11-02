928de7b8b9e8ab7bab315f4ae2d88f32465662af
dark0dave <dark0dave@mykolab.com>
2023-11-02 02:03:14 +0000

    eat(safer): Leaker
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- src/array.rs
+++ src/array.rs
@@ -1,4 +1,4 @@
-use std::{alloc::Layout, fmt::Debug, marker::PhantomData, mem::ManuallyDrop};
+use std::{fmt::Debug, marker::PhantomData, mem::ManuallyDrop, ptr::NonNull};
 
 use serde::{de::Visitor, Deserialize, Serialize};
 
@@ -7,63 +7,25 @@ use crate::iter::Iter;
 #[derive(Eq, PartialOrd, Ord)]
 #[repr(C, packed)]
 pub struct Array<T: Serialize + for<'a> Deserialize<'a> + Copy> {
-    pub(crate) pointer: *mut T,
+    pub(crate) pointer: NonNull<T>,
     pub(crate) size: usize,
 }
 
 impl<T: ?Sized + Serialize + for<'a> Deserialize<'a> + Copy> Array<T> {
     #[inline]
     pub fn new(size: usize) -> Array<T> {
-        let layout = match Layout::array::<T>(size) {
-            Ok(layout) => layout,
-            Err(_) => panic!("Could not create layout"),
-        };
-        unsafe {
-            let pointer = std::alloc::alloc(layout).cast();
-            Self { pointer, size }
+        Self {
+            pointer: NonNull::dangling(),
+            size,
         }
     }
     #[inline]
     pub fn iter(&self) -> Iter<T> {
         Iter::new(self)
     }
-    /// # Safety
-    ///
-    /// Get pointer to index
-    #[inline]
-    pub unsafe fn get_ptr(&self, index: usize) -> *const T {
-        self.pointer.add(index)
-    }
-    /// # Safety
-    ///
-    /// Get a mutable pointer to index
-    #[inline]
-    pub unsafe fn get_mut_ptr(&self, index: usize) -> *mut T {
-        self.pointer.add(index)
-    }
-    /// # Safety
-    ///
-    /// An unchecked get
-    #[inline]
-    pub unsafe fn get_unchecked(&self, index: usize) -> &T {
-        &(*(self.pointer.add(index)))
-    }
     #[inline]
     pub fn get(&self, index: usize) -> Option<&T> {
-        if self.size <= index {
-            None
-        } else {
-            unsafe { Some(self.get_unchecked(index)) }
-        }
-    }
-    /// # Safety
-    ///
-    /// This method does not check the index bounds,
-    /// so it's more efficient,
-    /// but can produce undefined behaviour
-    #[inline]
-    pub unsafe fn get_mut(&mut self, index: usize) -> &mut T {
-        &mut (*(self.pointer.add(index)))
+        self.iter().nth(index)
     }
 }
 
@@ -71,7 +33,7 @@ impl<T: Serialize + for<'a> Deserialize<'a> + Copy + Clone> Clone for Array<T> {
     fn clone(&self) -> Self {
         let out = Self::new(self.size);
         unsafe {
-            std::ptr::copy_nonoverlapping(self.pointer, out.pointer, self.size);
+            std::ptr::copy_nonoverlapping(self.pointer.as_ptr(), out.pointer.as_ptr(), self.size);
         }
         out
     }
@@ -80,7 +42,10 @@ impl<T: Serialize + for<'a> Deserialize<'a> + Copy + Clone> Clone for Array<T> {
 impl<T: Serialize + for<'a> Deserialize<'a> + Copy> Drop for Array<T> {
     fn drop(&mut self) {
         unsafe {
-            std::ptr::drop_in_place(std::ptr::slice_from_raw_parts_mut(self.pointer, self.size))
+            std::ptr::drop_in_place(std::ptr::slice_from_raw_parts_mut(
+                self.pointer.as_ptr(),
+                self.size,
+            ))
         }
     }
 }
@@ -93,20 +58,33 @@ impl<'b, T: Serialize + for<'a> Deserialize<'a> + Copy> IntoIterator for &'b Arr
     }
 }
 
-impl<T: Serialize + for<'a> Deserialize<'a> + Copy> From<Vec<T>> for Array<T> {
-    fn from(vec: Vec<T>) -> Self {
-        let size = vec.len();
-        let vec = ManuallyDrop::new(vec);
-        let mut array: Array<T> = Array::new(size);
-        let mut i = 0_usize;
+impl<T: Serialize + for<'a> Deserialize<'a> + Copy> From<Box<[T]>> for Array<T> {
+    #[inline]
+    fn from(value: Box<[T]>) -> Self {
+        let parts = Box::into_raw(value);
         unsafe {
-            let (_head, aligned, _tail) = vec.align_to::<T>();
-            for item in aligned {
-                *array.get_mut(i) = *item;
-                i += 1
+            Self {
+                pointer: NonNull::new_unchecked(parts.cast()),
+                size: (*parts).len(),
             }
         }
-        array
+    }
+}
+
+impl<T: Serialize + for<'a> Deserialize<'a> + Copy> From<Array<T>> for Vec<T> {
+    #[inline]
+    fn from(vec: Array<T>) -> Self {
+        let value = ManuallyDrop::new(vec);
+        unsafe { Vec::from_raw_parts(value.pointer.as_ptr(), value.size, value.size) }
+    }
+}
+
+impl<T: Serialize + for<'a> Deserialize<'a> + Copy> From<Array<T>> for Box<[T]> {
+    #[inline]
+    fn from(vec: Array<T>) -> Self {
+        let array = ManuallyDrop::new(vec);
+        let ptr = std::ptr::slice_from_raw_parts_mut(array.pointer.as_ptr(), array.size);
+        unsafe { Box::from_raw(ptr) }
     }
 }
 
@@ -135,7 +113,7 @@ impl<'de, T: Serialize + for<'a> Deserialize<'a> + 'de + Copy> Deserialize<'de> 
                 while let Ok(Some(item)) = seq.next_element::<T>() {
                     destination.push(item);
                 }
-                Ok(Array::from(destination))
+                Ok(Array::from(destination.into_boxed_slice()))
             }
         }
 
@@ -273,11 +251,12 @@ mod tests {
                     e: i as u128,
                     f: 1_i8,
                     g: i as i16,
-                    h: i,
+                    h: i as i32,
                     i: i as i64,
                     j: i as i128,
                 })
-                .collect::<Vec<Example>>(),
+                .collect::<Vec<Example>>()
+                .into_boxed_slice(),
         );
         let expected = ArrayExample(example);
 
@@ -322,7 +301,7 @@ mod tests {
         let example: Array<Example> = Array::from(
             (1..10)
                 .map(|i| Example {
-                    a: i,
+                    a: i as u8,
                     b: u16::MAX,
                     c: u32::MAX,
                     d: u64::MAX,
@@ -333,7 +312,8 @@ mod tests {
                     i: i64::MIN,
                     j: i128::MAX,
                 })
-                .collect::<Vec<Example>>(),
+                .collect::<Vec<Example>>()
+                .into_boxed_slice(),
         );
         let expected = StackArrayExample(ArrayExample(example));
         let result: StackArrayExample = copy_buff_to_struct(to_u8_slice(&expected));
@@ -356,7 +336,8 @@ mod tests {
                     i: i64::MIN,
                     j: i128::MAX,
                 })
-                .collect::<Vec<Example>>(),
+                .collect::<Vec<Example>>()
+                .into_boxed_slice(),
         );
         let expected = StackArrayExample(ArrayExample(example));
         let mut result: StackArrayExample = copy_buff_to_struct(to_u8_slice(&expected));
@@ -366,7 +347,7 @@ mod tests {
             result = copy_buff_to_struct(to_u8_slice(&result));
         }
         assert_eq!(
-            expected.0 .0.get(0),
+            result.0 .0.get(0),
             Some(&Example {
                 a: 1,
                 b: u16::MAX,
@@ -381,14 +362,14 @@ mod tests {
             })
         );
         assert_eq!(
-            result.0 .0.get(0),
+            result.0 .0.get(8),
             Some(&Example {
-                a: 1,
+                a: 9,
                 b: u16::MAX,
                 c: u32::MAX,
                 d: u64::MAX,
                 e: u128::MAX,
-                f: 1,
+                f: 9,
                 g: i16::MIN,
                 h: i32::MAX,
                 i: i64::MIN,
